6137f835810dd5ee5413bfedc45a8914dc33907b
Meredith Wang <wang.meredith09@gmail.com>
2022-06-30 09:57:12 -0500

    udpated function for iris dataset

--- acquire.py
+++ acquire.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import os
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
 def new_titanic_data():
     return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
-
-import os
 
 def get_titanic_data():
     filename = "titanic.csv"
@@ -26,31 +26,45 @@ def get_titanic_data():
         return df  
 
 def new_iris_data():
-    query = '''
-    SELECT * FROM species JOIN measurements USING(species_id)
     '''
-    return pd.read_sql(query, get_connection('iris_db'))
-
-import os
+    This function reads the iris data from the Codeup db into a df.
+    '''
+    sql_query = """
+                SELECT 
+                    species_id,
+                    species_name,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    
+    return df
 
 def get_iris_data():
-    filename = "iris.csv"
-    
-    # if file is available locally, read it
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    
-    # if file not available locally, acquire data from SQL database
-    # and write it as csv locally for future use
+    '''
+    This function reads in iris data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('iris_df.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('iris_df.csv', index_col=0)
+        
     else:
-        # read the SQL query into a dataframe
+        
+        # Read fresh data from db into a DataFrame
         df = new_iris_data()
         
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename)
-
-        # Return the dataframe to the calling code
-        return df  
+        # Cache data
+        df.to_csv('iris_df.csv')
+        
+    return df 
 
 def new_telco_data():
     query = '''
@@ -59,8 +73,6 @@ def new_telco_data():
     JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id;
     '''
     return pd.read_sql(query, get_connection('telco_churn'))
-
-import os
 
 def get_telco_data():
     filename = "telco.csv"
