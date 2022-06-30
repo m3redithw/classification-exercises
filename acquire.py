225711665d13d9e363be1739e0c12177b1d3280b
Meredith Wang <wang.meredith09@gmail.com>
2022-06-30 10:01:11 -0500

    udpated import

--- acquire.py
+++ acquire.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import env
+import seaborn as sns
+import requests
 import os
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
