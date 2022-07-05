b7532698206032fd396b7621992c718bc8f05474
Meredith Wang <wang.meredith09@gmail.com>
2022-07-05 13:48:43 -0500

    updated prepare file

--- prepare.py
+++ prepare.py
@@ -1,6 +1,12 @@
-from pandas import DataFrame
-from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import splitting and imputing functions
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+import acquire
 
 def handle_missing_values(df):
     return df.assign(
@@ -16,11 +22,29 @@ def encode_embarked(df):
     encoder.fit(df.embarked)
     return df.assign(embarked_encode = encoder.transform(df.embarked))
 
-def prep_titanic_data(df):
-    df = df\
-        .pipe(handle_missing_values)\
-        .pipe(remove_columns)\
-        .pipe(encode_embarked)
+def prep_titanic(df):
+
+    cols_to_drop = ['deck', 'embarked', 'class', 'age']
+    df = df.drop(columns=cols_to_drop)
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], dummy_na=False)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+def prep_iris(df):
+    cols_to_drop = ['species_id']
+    df = df.drop(columns=cols_to_drop)
+    df.rename(columns = {'species_name':'species'}, inplace = True)
+    dummy_df = pd.get_dummies(df[['species']], dummy_na= False)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+def prep_telco(df):
+    cols_to_drop = ['contract_type_id', 'customer_id', 'internet_service_type_id', 'payment_type_id', 'payment_type_id.1', 'internet_service_type_id', 'internet_service_type_id.1']
+    df = df.drop(columns=cols_to_drop)
+    df.total_charges = df.total_charges.str.strip().replace('',0).astype(float)
+    dummy_df = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn', 'contract_type', 'payment_type', 'internet_service_type']], dummy_na=False, drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
     return df
 
 def train_validate_test_split(df, seed=123):
